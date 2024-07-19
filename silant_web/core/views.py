d3314ecbf94154ac5e8d2da76caac62bd00dd603
Test <Test@test.test>
2024-07-19 17:39:30 +0700

    add unauthorized view

--- silant_web/core/views.py
+++ silant_web/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import (
+    ListView,
+)
+
+from .models import (
+    Machine,
+)
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        serial_number = request.POST.get('serial_number')
+        machine = Machine.objects.filter(serial_number_of_machine=serial_number).first()
+        if machine:
+            return render(request, 'index.html', {'machine': machine})
+        else:
+            return render(request, 'index.html', {'error_message': 'Machine not found'})
+    else:
+        return render(request, 'index.html')

81cf399df05aa279e7326530a1de50070131bc73
Test <Test@test.test>
2024-07-22 17:51:44 +0700

    add paths for list of machine and machine detail

--- silant_web/core/urls.py
+++ silant_web/core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from .views import (
-    index,
+    unauthorized_index,
+    machine_list,
+    machine_detail,
 )
 
 urlpatterns = [
-    path('', index, name='posts'),
+    path('', unauthorized_index, name='welcome'),
+    path('machines/', machine_list, name='machine_list'),
+    path('machines/<int:machine_id>/', machine_detail, name='machine_detail'),
 ]

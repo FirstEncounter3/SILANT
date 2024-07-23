51f5507b5b697eae8209ff3ecd559128184156ba
Test <Test@test.test>
2024-07-23 17:33:00 +0700

    add route to complaints list

--- silant_web/core/urls.py
+++ silant_web/core/urls.py
@@ -4,10 +4,12 @@ from .views import (
     unauthorized_index,
     machine_list,
     machine_detail,
+    complaints_list,
 )
 
 urlpatterns = [
     path('', unauthorized_index, name='welcome'),
     path('machines/', machine_list, name='machine_list'),
     path('machines/<int:machine_id>/', machine_detail, name='machine_detail'),
+    path('complaints/', complaints_list, name='complaints_list'),
 ]

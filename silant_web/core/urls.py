04e405c0a6266dd6a746c26fc6ceaeff3cbe7fab
Test <Test@test.test>
2024-07-24 17:18:51 +0700

    add routes for maintenances list and create form

--- silant_web/core/urls.py
+++ silant_web/core/urls.py
@@ -5,6 +5,8 @@ from .views import (
     machine_list,
     machine_detail,
     complaints_list,
+    maintenance_list,
+    MaintenanceCreateView,
 )
 
 urlpatterns = [
@@ -12,4 +14,6 @@ urlpatterns = [
     path('machines/', machine_list, name='machine_list'),
     path('machines/<int:machine_id>/', machine_detail, name='machine_detail'),
     path('complaints/', complaints_list, name='complaints_list'),
+    path('maintenances/', maintenance_list, name='maintenance_list'),
+    path('maintenances/create/', MaintenanceCreateView.as_view(), name='maintenance_create'),
 ]

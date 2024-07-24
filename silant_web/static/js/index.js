383c5e156ea274ec296172172c77e2b38f0b83ce
Test <Test@test.test>
2024-07-24 17:20:39 +0700

    small refactoring, add new button event listener

--- silant_web/static/js/index.js
+++ silant_web/static/js/index.js
@@ -1,14 +1,21 @@
 const complaintButton = document.getElementById('complaint-button');
 const generalButton = document.getElementById('general-button');
 const maintenanceButton = document.getElementById('maintenance-button');
+const createMaintenanceButton = document.getElementById('create_maintenance');
 
 
 complaintButton.addEventListener('click', () => {
     window.location.href = '/complaints/';
 });
+
 generalButton.addEventListener('click', () => {
     window.location.href = '/machines/';
 });
+
 maintenanceButton.addEventListener('click', () => {
-    window.location.href = '/machines/';
+    window.location.href = '/maintenances/';
+})
+
+createMaintenanceButton.addEventListener('click', () => {
+    window.location.href = '/maintenances/create/';
 })

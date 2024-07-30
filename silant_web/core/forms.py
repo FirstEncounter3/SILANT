8c63406eab9233435675cc2e2abc4185e131230a
Test <Test@test.test>
2024-07-30 17:22:08 +0700

    add create machine form

--- silant_web/core/forms.py
+++ silant_web/core/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import (
     Maintenance,
     Complaint,
+    Machine,
 )
 
 
@@ -42,4 +43,31 @@ class ComplaintForm(forms.ModelForm):
         widgets = {
             "date_of_refusal": forms.DateInput(attrs={"type": "date"}),
             "recovery_date": forms.DateInput(attrs={"type": "date"}),
+        }
+
+
+class MachineForm(forms.ModelForm):
+    class Meta:
+        model = Machine
+        fields = [
+            'serial_number_of_machine',
+            'model_of_equipment',
+            'model_of_engine',
+            'serial_number_of_engine',
+            'transmission_model',
+            'serial_number_of_transmission',
+            'drive_axle_model',
+            'serial_number_of_drive_axle',
+            'steering_axle_model',
+            'serial_number_of_steering_axle',
+            'supply_contract_number_and_date',
+            'date_shipped_from_factory',
+            'recipient',
+            'delivery_address',
+            'equipment',
+            'client',
+            'service_company',
+        ]
+        widgets = {
+            'date_shipped_from_factory': forms.DateInput(attrs={'type': 'date'}),
         }

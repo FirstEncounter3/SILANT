5a5f68c4b2491432068738f0d2e26f3def02d629
Test <Test@test.test>
2024-07-31 17:13:26 +0700

    add route to update machine view

--- silant_web/core/urls.py
+++ silant_web/core/urls.py
@@ -9,12 +9,14 @@ from .views import (
     MaintenanceCreateView,
     ComplaintCreateView,
     MachineCreateView,
+    MachineUpdateView,
 )
 
 urlpatterns = [
     path('', unauthorized_index, name='welcome'),
     path('machines/', machine_list, name='machine_list'),
     path('machines/create/', MachineCreateView.as_view(), name='machine_create'),
+    path('machines/update/<int:pk>/', MachineUpdateView.as_view(), name='machine_update'),
     path('machines/<int:machine_id>/', machine_detail, name='machine_detail'),
     path('complaints/', complaints_list, name='complaints_list'),
     path('maintenances/', maintenance_list, name='maintenance_list'),

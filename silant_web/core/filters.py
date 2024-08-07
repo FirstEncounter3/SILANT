b78ae939b5b4954b798d1f78698e2ccc01b5f4d8
First_Encounter <needenergy27@gmail.com>
2024-08-08 01:48:07 +0700

    add new filter filed

--- silant_web/core/filters.py
+++ silant_web/core/filters.py
@@ -19,19 +19,23 @@ class MachineFilter(django_filters.FilterSet):
         ]
 
 class MaintenanceFilter(django_filters.FilterSet):
+    serial_number_of_machine = django_filters.CharFilter(field_name='machine__serial_number_of_machine')
     class Meta:
         model = Maintenance
         fields = [
             'maintenance_type',
             'service_company',
+            'serial_number_of_machine',
         ]
 
 
 class ComplaintFilter(django_filters.FilterSet):
+    serial_number_of_machine = django_filters.CharFilter(field_name='machine__serial_number_of_machine')
     class Meta:
         model = Complaint
         fields = [
             'failure_node',
             'recovery_method',
             'service_company',
+            'serial_number_of_machine',
         ]
